7a582983245926ee70c8c362bf15fe66b7eaa023
Cong Nguyen <rampadc@users.noreply.github.com>
2023-10-01 19:26:23 +1100

    uart0 and uart1 can talk to each other

--- src/main.c
+++ src/main.c
@@ -7,12 +7,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart.h>
-#include <zephyr/logging/log.h>
 #include <string.h>
 
 int read_uart_most_recent(uint8_t *msg, int msg_size);
-
-LOG_MODULE_REGISTER(nrf53_uart_async, LOG_LEVEL_DBG);
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -25,7 +22,9 @@ LOG_MODULE_REGISTER(nrf53_uart_async, LOG_LEVEL_DBG);
 K_MSGQ_DEFINE(modem_msgq, MSG_SIZE, 10, 4);
 
 // receive buffer for modem's UART
-static char rx_buf[MSG_SIZE] = {0};
+static char uart1_rx_buf[MSG_SIZE] = {0};
+static char uart0_rx_buf[MSG_SIZE] = {0};
+static char printout[MSG_SIZE] = {0};
 
 /*
  * A build error on this line means your board is unsupported.
@@ -34,7 +33,9 @@ static char rx_buf[MSG_SIZE] = {0};
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 // Use UART1 to communicate with modem
-static const struct device *modem = DEVICE_DT_GET(DT_NODELABEL(uart1));
+static const struct device *uart1 = DEVICE_DT_GET(DT_NODELABEL(uart1));
+static const struct device *uart0 = DEVICE_DT_GET(DT_NODELABEL(uart0));
+
 const struct uart_config uart_cfg = {
 	.baudrate = 115200,
 	.parity = UART_CFG_PARITY_NONE,
@@ -43,35 +44,19 @@ const struct uart_config uart_cfg = {
 	.flow_ctrl = UART_CFG_FLOW_CTRL_NONE,
 };
 
-static void modem_uart_cb(const struct device *dev, struct uart_event *event, void *user_data) {
+void uart_write(const struct device *dev, char *buf);
+
+static void uart1_cb(const struct device *dev, struct uart_event *event, void *user_data) {
 	switch (event->type) {
 		case UART_RX_RDY:
 			// received data is ready for prcessing
-			// LOG_INF("rx_buf: %s\r\n", (char *)event->data.rx_buf.buf);
-
-			size_t length = event->data.rx.len;
-			size_t offset = event->data.rx.offset;
-			char s[MSG_SIZE];
-			memcpy(s, event->data.rx_buf.buf + offset, length);
-			s[event->data.rx.len] = '\0';
-
-			// remove new lines
-			for (size_t i = 0; i < event->data.rx.len; i++) {
-				if (s[i] == '\n' || s[i] == '\r') {
-					s[i] = '\0';
-					break;
-				}
-			}
-
-			k_msgq_put(&modem_msgq, &s, K_NO_WAIT);
-
-			LOG_INF("msg: %s\r\n", s);
+			printk("uart1: %s", (char *)event->data.rx_buf.buf);
 			break;
 
 		// Continuous reception is not enabled by default, which means once the receive buffer is full, 
 		// you must manually re-enable UART to enable reception
 		case UART_RX_DISABLED:
-			uart_rx_enable(dev, rx_buf, sizeof(rx_buf), 100);
+			uart_rx_enable(dev, uart1_rx_buf, sizeof(uart1_rx_buf), 100);
 			break;
 		
 		default:
@@ -79,18 +64,32 @@ static void modem_uart_cb(const struct device *dev, struct uart_event *event, vo
 	}
 }
 
-void modem_read(char *buf) {
-	
+static void uart0_cb(const struct device *dev, struct uart_event *event, void *user_data) {
+	switch (event->type) {
+		case UART_RX_RDY:
+			uart_tx(uart1, event->data.rx.buf + event->data.rx.offset, event->data.rx.len, 100);
+			// snprintk(printout, event->data.rx.len, "%s", event->data.rx.buf);
+			// uart_write(uart1, printout);
+			break;
+
+		// Continuous reception is not enabled by default, which means once the receive buffer is full, 
+		// you must manually re-enable UART to enable reception
+		case UART_RX_DISABLED:
+			uart_rx_enable(dev, uart1_rx_buf, sizeof(uart1_rx_buf), 100);
+			break;
+		
+		default:
+			break;
+	}
 }
 
-void modem_write(const struct device *dev, char *buf) {
+void uart_write(const struct device *dev, char *buf) {
 	int msg_len = strlen(buf);
 
 	for (int i = 0; i < msg_len; i++) {
 		uart_tx(dev, buf, msg_len, SYS_FOREVER_US);
 	}
 }
-
 
 int main(void)
 {
@@ -101,41 +100,50 @@ int main(void)
 		return 0;
 	}
 
-	if (!device_is_ready(modem)) {
+	if (!device_is_ready(uart1)) {
 		return 0;
 	}
 
-	LOG_INF("Configuring UART\r\n");
-	err = uart_configure(modem, &uart_cfg);
+	printk("Configuring UART\r\n");
+	err = uart_configure(uart1, &uart_cfg);
 	if (err) {
-		LOG_ERR("uart_configure() failed. Error: %d\r\n", err);
+		printk("uart_configure() failed. Error: %d\r\n", err);
 		return err;
 	}
 
-	err = uart_callback_set(modem, modem_uart_cb, NULL);
+	err = uart_callback_set(uart1, uart1_cb, NULL);
 	if (err) {
-		LOG_ERR("Cannot register uart callback. Error: %d\r\n", err);
+		printk("Cannot register uart callback. Error: %d\r\n", err);
 		return err;
 	}
-	LOG_INF("UART callback registered\r\n");
+	printk("UART1 callback registered\r\n");
 
-	err = uart_rx_enable(modem, rx_buf, sizeof(rx_buf), 100);
+	err = uart_callback_set(uart0, uart0_cb, NULL);
 	if (err) {
-		LOG_ERR("UART RX failed to enable. Error: %d\r\n", err);
+		printk("Cannot register uart0 callback. Error: %d\r\n", err);
+	}
+
+	err = uart_rx_enable(uart1, uart1_rx_buf, sizeof(uart1_rx_buf), 100);
+	if (err) {
+		printk("UART1 RX failed to enable. Error: %d\r\n", err);
 		return err;
 	}
-	LOG_INF("UART RX enabled\r\n");
+	printk("UART1 RX enabled\r\n");
+
+	err = uart_rx_enable(uart0, uart0_rx_buf, sizeof(uart0_rx_buf), 100);
+	if (err) {
+		printk("UART0 RX failed to enable. Error: %d\r\n", err);
+		return err;
+	}
+	printk("UART0 RX enabled\r\n");
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
+	printk("UART0 (Zephyr Shell) says hello\r\n");
+	uart_write(uart1, "UART1 says hello\r\n");
 
-	modem_write(modem, "Device started.\r\n");
-
-	char received_modem_buf[MSG_SIZE];
-	while (k_msgq_get(&modem_msgq, &received_modem_buf, K_FOREVER) == 0) {
-		LOG_INF("q: %s\r\n", received_modem_buf);
-	}
+	k_yield();
 	return 0;
 }
